197fda31585f9e73276aaa5dba911150ab7f3dad
Chris Holdgraf <choldgraf@berkeley.edu>
2016-11-29 17:41:26 -0800

    updating conversion tool and the css file

--- jupyter_to_wordpress.py
+++ jupyter_to_wordpress.py
@@ -4,8 +4,9 @@ from os import path as op
 import shutil as sh
 from glob import glob
 import time
+import nbformat as nb
 
-file_path = sys.argv[1]
+file_path = op.abspath(sys.argv[1]).replace(' ', '\ ')
 
 root = op.dirname(file_path)
 if root == '':
@@ -34,12 +35,53 @@ else:
     day = now.tm_mday
     year = now.tm_year
 
+# Clean up the notebook
+file_path_tmp = file_path + '_TMP'
+ntbk = nb.read(file_path, nb.NO_CONVERT)
+new_cells = []
+for ii, cell in enumerate(ntbk.cells):
+    # Don't modify non-code cells and just add
+    if cell['cell_type'] != 'code':
+        new_cells.append(cell)
+        continue
+
+    # Skip the cell if it's empty
+    if len(cell['source']) == 0:
+        continue
+
+    # Clean outputs otherwise
+    outputs = []
+    for output in cell['outputs']:
+        # Remove stderrors in the outputs
+        if 'name' in list(output.keys()):
+            if output['name'] != 'sterr':
+                continue
+        # Check if we have any object outputs (e.g. a function returned)
+        if 'data' in list(output.keys()):
+            if 'text/plain' in output['data'].keys():
+                if output['data']['text/plain'].startswith('<'):
+                    _ = output['data'].pop('text/plain')
+        outputs.append(output)
+    cell['outputs'] = outputs
+    cell['execution_count'] = None
+    new_cells.append(cell)
+
+# Update the notebook w/ new cells and write it
+ntbk['cells'] = new_cells
+nb.write(ntbk, file_path_tmp, nb.NO_CONVERT)
+
 # Now convert to html and move to the `html` folder
-cmd_convert = 'jupyter nbconvert --to html --template basic {}'.format(file_path)
+curdir = os.path.abspath(os.curdir)
+newdir = os.path.dirname(__file__)
+cmd_convert = 'jupyter nbconvert --to html --template basic {}'.format(file_path_tmp)
 print("Running command:\n{}".format(cmd_convert))
+os.chdir(newdir)
 os.system(cmd_convert)
+os.chdir(curdir)
+
 new_folder = '{}/html/'.format(root)
 new_file = '{}.html'.format(title)
 html_file = '{}-{}-{}-{}.html'.format(year, mo, day, title)
 sh.move('{}/{}'.format(root, new_file), '{}{}'.format(new_folder, html_file))
+os.remove(file_path_tmp)
 print('New file is at:\n{}'.format(op.abspath(new_folder + html_file)))
